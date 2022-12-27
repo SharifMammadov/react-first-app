ac681aeb2cafcb995efac90138861ab08fd77dbe
Memmedov <sherif_memmedov@winadpro.com>
2022-12-27 13:28:43 +0400

    first-branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+const language  ="English";
+function App() {
+  return (
+    <div className="App">
+      <p>Dil: {language}
+        
+      </p>
+    </div>
+  );
+}
+
+export default App;
